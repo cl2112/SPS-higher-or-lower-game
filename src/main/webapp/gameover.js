ed530226d23df44571b22a28366acea037ce17fc
navila-luna <navila@uchicago.edu>
2021-07-19 03:37:06 +0000

    refactored code

--- src/main/webapp/gameover.js
+++ src/main/webapp/gameover.js
@@ -1,27 +1,42 @@
 
 // Wait for the page to load then call the startGame function.
-document.addEventListener("DOMContentLoaded", getAndSaveScore);
+document.addEventListener("DOMContentLoaded", displayResults);
 
-function getAndSaveScore() {
-  if (localStorage.getItem("finalScore") == null) {
+// Returns player's final game score and deletes it from local storage
+function getScore() {
+  // Get score from finalscore
+  let score = localStorage.getItem("finalScore");
+  // Clear everything from finalscore to prevent reusing
+  localStorage.removeItem("finalScore");
+  if (score == null) {
     return;
   }
-  let score = Number(localStorage.getItem("finalScore"));
+  return Number(score); 
+}
+
+// Gets player's average score and updates avgScore based on last score
+function getAvg(score) {
   let avgScore = localStorage.getItem("avgScore");
-  if (avgScore == null) {
+
+  // If nothing in avgScore, creates avgScore object and sets game count to 1
+  if (avgScore === null) {
     avgScore = {sum: score, count: 1};
   }
+  // Updates score and number of games in avgScore
   else {
     avgScore = JSON.parse(avgScore);
     avgScore.sum += score;
     avgScore.count += 1;
   }
-  console.log("sum", avgScore.sum);
-  console.log("count", avgScore.count);
-  localStorage.setItem("avgScore", JSON.stringify(avgScore)); 
-  let avg = avgScore.sum / avgScore.count;
-  console.log("avg", avg);
+  // Adds avgScore to local storage and return avg score
+  localStorage.setItem("avgScore", JSON.stringify(avgScore));
+  return avgScore.sum / avgScore.count;
+}
+
+// Displays the average player score and the user final score at gameover 
+function displayResults() {
+  let score = getScore();
+  let avg = getAvg(score);
   document.getElementById("results-average").innerText = avg;
   document.getElementById("results-score").innerText = score;
-  localStorage.clear("finalScore");
 }
